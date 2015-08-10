c636349fc3cf66db1ef3fa0ef5792d7328c7dac6
Donna Yu <Donnayu1998@gmail.com>
2015-08-10 17:22:49 -0400

    Score and game over

--- SaveTheRain.spritebuilder/Source/MainScene.swift
+++ SaveTheRain.spritebuilder/Source/MainScene.swift
@@ -7,6 +7,8 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
     //talk about raindrops
     var rainDrops: [RainDrop] = []
     weak var rainDrop: CCSprite!
+    weak var sand: CCSprite!
+
     var dropsCollected: Int = 0
     let firstRainDropPosition : CGFloat = 280
     let distanceBetweenRainDrops : CGFloat = 160
@@ -19,6 +21,8 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
     var gameOver = false
     var counter = 0
     weak var gamePhysicsNode: CCPhysicsNode!
+    weak var scoreLabel: CCLabelTTF!
+    weak var restartButton : CCButton!
     
     
     // FUNCTIONS
@@ -34,17 +38,22 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
         spawnNewRainDrop()
     }
     
-    // TRIGGERS GAME OVER
-    func triggerGameOver() {
-        if (gameOver == true) {
-            println("Todo: gameover")
-        }
-    }
+//    // TRIGGERS GAME OVER
+//    func triggerGameOver() {
+//        if (gameOver == true) {
+//            println("Todo: gameover")
+//        }
+//    }
     
     //CALLED WHEN YOU TOUCH THE SCREEN
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
         var touchLocation = touch.locationInWorld()
         bucket.position.x = touchLocation.x
+        if (gameOver == false) {
+            bucket.physicsBody.applyImpulse(ccp(0, 400))
+            bucket.physicsBody.applyAngularImpulse(10000)
+//            sinceTouch = 0
+        }
     }
     
     //CALLED WHEN YOU DRAG YOUR FINGER ON THE SCREEN
@@ -102,9 +111,38 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
     
     //COLLISION TESTING
     func ccPhysicsCollisionBegin(pair: CCPhysicsCollisionPair!, bucket: Bucket!, raindrop: RainDrop!) -> Bool {
-        raindrop.removeFromParent()
+        if raindrop != nil { raindrop.removeFromParent() }
         dropsCollected++
+        scoreLabel.string = String(dropsCollected)
         return false
     }
     
+    func ccPhysicsCollisionBegin(pair: CCPhysicsCollisionPair!, sand: CCSprite!, raindrop: RainDrop!) -> Bool {
+        triggerGameOver()
+        if raindrop != nil { raindrop.removeFromParent() }
+        print ("HI")
+        return true
+    }
+    
+    func restart() {
+        let scene = CCBReader.loadAsScene("MainScene")
+        CCDirector.sharedDirector().presentScene(scene)
+    }
+    
+    func triggerGameOver() {
+        if (gameOver == false) {
+            gameOver = true
+            restartButton.visible = true
+//            rainDrop = 0
+            isTouchingBucket = false
+            
+            // just in case
+//            rainDrop.stopAllActions()
+            
+            let move = CCActionEaseBounceOut(action: CCActionMoveBy(duration: 0.2, position: ccp(0, 4)))
+            let moveBack = CCActionEaseBounceOut(action: move.reverse())
+            let shakeSequence = CCActionSequence(array: [move, moveBack])
+            runAction(shakeSequence)
+        }
+    }
 }
